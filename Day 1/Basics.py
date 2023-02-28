86ecb210c26de535f542aa8b1f0c56442ba0e7d0
Sugu2023 <123944031+Sugu2023@users.noreply.github.com>
2023-02-28 18:18:23 +0530

    Create Basics.py

---
+++ Day 1/Basics.py
@@ -0,0 +1,40 @@
+>>> #NUMBER
+>>> 2+3
+5
+>>> 2+(4*2)
+10
+>>> 4+5/3
+5.666666666666667
+>>> #division always returns a floating point number
+>>> 10/2
+5.0
+>>> 10//2
+5
+>>> #floor devision only discards fractional parts
+>>> 10%3 #the % symbol it return the remainder
+1
+>>> #We can calculate the power of a number using **operator
+>>> 5**2
+25
+>>> 2**8
+256
+>>> #using = we can assign a value to the variable
+>>> a=10
+>>> a
+10
+>>> a
+10
+>>> #if we didnt define a value to varibale and we trying to access means we get error as 'variable' is not defined.
+>>> n
+Traceback (most recent call last):
+  File "/usr/lib/python3.8/idlelib/run.py", line 559, in runcode
+    exec(code, self.locals)
+  File "<pyshell#17>", line 1, in <module>
+NameError: name 'n' is not defined
+>>> 4*2.75
+11.0
+>>> 4*3.75-1
+14.0
+>>> 4*3.75
+15.0
+>>> 
